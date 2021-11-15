db41643198cc024c2e5144cf74ca44c6e6442bbe
Selahattin Topaloğlu <selahattin.topaloglu@guralporselen.com.tr>
2021-11-15 11:27:54 +0300

    Random Password Creator

---
+++ Console/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Text;
+
+namespace Console
+{
+    public class Program
+    {
+        private const string CharactersU = "QWERTYUOPASDFGHJKLZXCVBNM";
+        private const string CharactersL = "qwertyuiopasdfghjklizxcvbnm";
+        private const string SpecialCharacters = "!.";
+        private const string Numbers = "1234567890";
+        private const string AllCharacters = CharactersU + CharactersL + SpecialCharacters;
+        private static Random rnd = new Random();
+        static void Main()
+        {
+            StringBuilder password = new StringBuilder();
+            for (int i = 0; i < 1; i++)
+            {
+                char capitalLetters = GenerateChar(CharactersU);
+                InsertAtRandomPositions(password, capitalLetters);
+                
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                char smallLetter = GenerateChar(CharactersL);
+                InsertAtRandomPositions(password, smallLetter);
+            }
+            for (int i = 0; i<2; i++)
+            {
+                char number = GenerateChar(Numbers);
+                InsertAtRandomPositions(password, number);
+            }
+
+            for (int i = 0; i<1; i++)
+            {
+                char special = GenerateChar(SpecialCharacters);
+                InsertAtRandomPositions(password, special);
+            }
+          
+            
+            System.Console.WriteLine(password);
+            System.Console.ReadLine();
+        }   
+
+        private static void InsertAtRandomPositions(StringBuilder password , char character)
+        {
+
+            int randomPosition = rnd.Next(password.Length + 1);
+            randomPosition = NewMethod(password, character, randomPosition);
+
+            password.Insert(randomPosition, character);
+
+          
+        }
+
+        private static int NewMethod(StringBuilder password, char character, int randomPosition)
+        {
+            if (randomPosition == 0)
+                if (character.Equals("!") || character.Equals("."))
+                {
+                    randomPosition = rnd.Next(password.Length + 1);
+                }
+
+            return randomPosition;
+        }
+
+        private static char GenerateChar (string avaibleChars)
+        {
+            int randomIndex = rnd.Next(avaibleChars.Length);
+            char randomChar = avaibleChars[randomIndex];
+            return randomChar;
+        }
+    }
+}
---
+++ RandomPassword/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace RandomPassword
+{
+    public class Class1
+    {
+        public string CreatePassword(int length)
+        {
+            const string valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            StringBuilder res = new StringBuilder();
+            Random rnd = new Random();
+            while (0 < length--)
+            {
+                res.Append(valid[rnd.Next(valid.Length)]);
+            }
+            return res.ToString();
+
+        Console.WriteLine(rnd);
+        }
+    }
+}
